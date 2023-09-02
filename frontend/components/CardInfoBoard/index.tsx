f37434608da687c9e62eb9a647e23f849669beb1
KeefSteef <deazy01025@gmail.com>
2023-09-02 17:52:18 +0300

    Create Task Board title validation added

--- frontend/components/CardInfoBoard/index.tsx
+++ frontend/components/CardInfoBoard/index.tsx
@@ -1,106 +1,103 @@
 import cls from './CardInfoBoard.module.scss'
 import Image from 'next/image'
-import {
-  useApiCardInfoContext,
-  useStateCardInfoContext,
-} from '../../contexts/CardInfoContext/useCardInfoContext'
-import { useCardInfoBoard } from './hooks/useCardnfoBoard'
-import { parsePxToTime } from '../../utils'
-import React, { FC, RefObject, useRef } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-import { ICardInfo } from '../../types/pages/Calendar'
+import {useApiCardInfoContext, useStateCardInfoContext,} from '../../contexts/CardInfoContext/useCardInfoContext'
+import {useCardInfoBoard} from './hooks/useCardnfoBoard'
+import {parsePxToTime} from '../../utils'
+import React, {FC, RefObject, useRef} from 'react'
+import {AnimatePresence, motion} from 'framer-motion'
+import {ICardInfo} from '../../types/pages/Calendar'
 
-const CardInfoBoard: FC<{ days: string[] }> = ({ days }) => {
-  const state = useStateCardInfoContext() as ICardInfo
-  const api = useApiCardInfoContext()
-  const prevDays = useRef(days)
-  const { enableBoard, disableBoard, removeCard, doCompleteCard } = useCardInfoBoard(state, api)
-  const infoBoard = useRef<HTMLElement>(null)
+const CardInfoBoard: FC<{ days: string[] }> = ({days}) => {
+    const state = useStateCardInfoContext() as ICardInfo
+    const api = useApiCardInfoContext()
+    const prevDays = useRef(days)
+    const {enableBoard, disableBoard, removeCard, doCompleteCard} = useCardInfoBoard(state, api)
+    const infoBoard = useRef<HTMLElement>(null)
 
-  const isShow = prevDays.current[0] === days[0]
-  prevDays.current = days
+    const isShow = prevDays.current[0] === days[0]
+    prevDays.current = days
 
-  return (
-    <div>
-      <AnimatePresence mode={'wait'}>
-        {state.name && isShow && (
-          <motion.div
-            ref={infoBoard as RefObject<HTMLDivElement>}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 20 }}
-            transition={{ duration: 0.1 }}
-            className={cls.cardInfo_container}
-            id={'cardInfoContainer'}
-            style={{ ...enableBoard() }}
-          >
-            <div className={cls.cardInfoNav}>
-              <Image
-                width={'25px'}
-                onClick={() => doCompleteCard(state.id)}
-                height={'25px'}
-                src={'/done3.svg'}
-                alt={'done'}
-              />
-              <Image
-                onClick={removeCard}
-                width={'30px'}
-                height={'30px'}
-                src={'/trash.svg'}
-                alt={'trash'}
-              />
-              <svg
-                onClick={() => disableBoard()}
-                width="22"
-                height="22"
-                viewBox="0 0 37 38"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  y="3.76276"
-                  width="5"
-                  height="45.7742"
-                  rx="1"
-                  transform="rotate(-42.8743 0 3.76276)"
-                  fill="#000"
-                />
-                <rect
-                  x="32.6185"
-                  width="5"
-                  height="47"
-                  rx="1"
-                  transform="rotate(43.9485 32.6185 0)"
-                  fill="#000"
-                />
-              </svg>
-            </div>
-            <div className={cls.info_container}>
-              <div className={cls.title}>
-                <h3>{state.name}</h3>
-              </div>
-              <div className={cls.card_info}>
-                <div className={cls.icon_container}>
-                  <Image width={'30px'} height={'30px'} src={'/clock.svg'} alt={'clock'} />
-                  <p>
-                    {parsePxToTime(state.cardTop)}-{parsePxToTime(state.cardTop + state.cardSize)}
-                  </p>
-                </div>
-                <div className={cls.icon_container}>
-                  <Image
-                    width={'30px'}
-                    height={'30px'}
-                    src={'/calendar_day.svg'}
-                    alt={'calendar'}
-                  />
-                  <p>{state.columnDay}</p>
-                </div>
-              </div>
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  )
+    return (
+        <div>
+            <AnimatePresence mode={'wait'}>
+                {state.name && isShow && (
+                    <motion.div
+                        ref={infoBoard as RefObject<HTMLDivElement>}
+                        initial={{opacity: 0, y: 20}}
+                        animate={{opacity: 1, y: 0}}
+                        exit={{opacity: 0, y: 20}}
+                        transition={{duration: 0.1}}
+                        className={cls.cardInfo_container}
+                        id={'cardInfoContainer'}
+                        style={{...enableBoard()}}
+                    >
+                        <div className={cls.cardInfoNav}>
+                            <Image
+                                width={'25px'}
+                                onClick={() => doCompleteCard(state.id)}
+                                height={'25px'}
+                                src={'/done3.svg'}
+                                alt={'done'}
+                            />
+                            <Image
+                                onClick={removeCard}
+                                width={'30px'}
+                                height={'30px'}
+                                src={'/trash.svg'}
+                                alt={'trash'}
+                            />
+                            <svg
+                                onClick={() => disableBoard()}
+                                width="22"
+                                height="22"
+                                viewBox="0 0 37 38"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <rect
+                                    y="3.76276"
+                                    width="5"
+                                    height="45.7742"
+                                    rx="1"
+                                    transform="rotate(-42.8743 0 3.76276)"
+                                    fill="#000"
+                                />
+                                <rect
+                                    x="32.6185"
+                                    width="5"
+                                    height="47"
+                                    rx="1"
+                                    transform="rotate(43.9485 32.6185 0)"
+                                    fill="#000"
+                                />
+                            </svg>
+                        </div>
+                        <div className={cls.info_container}>
+                            <div className={cls.title}>
+                                <h3>{state.name}</h3>
+                            </div>
+                            <div className={cls.card_info}>
+                                <div className={cls.icon_container}>
+                                    <Image width={'30px'} height={'30px'} src={'/clock.svg'} alt={'clock'}/>
+                                    <p>
+                                        {parsePxToTime(state.cardTop)}-{parsePxToTime(state.cardTop + state.cardSize)}
+                                    </p>
+                                </div>
+                                <div className={cls.icon_container}>
+                                    <Image
+                                        width={'30px'}
+                                        height={'30px'}
+                                        src={'/calendar_day.svg'}
+                                        alt={'calendar'}
+                                    />
+                                    <p>{state.columnDay}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div>
+    )
 }
 
 export default CardInfoBoard
--- frontend/components/CreateTaskBoard/hooks/useCreateTask.ts
+++ frontend/components/CreateTaskBoard/hooks/useCreateTask.ts
@@ -1,32 +1,33 @@
-import { useMutation } from '@apollo/client'
-import { CREATE_TASK_MUTATION } from '../../../graphql/Mutations'
-import { FormEvent } from 'react'
+import {useMutation} from '@apollo/client'
+import {CREATE_TASK_MUTATION} from '../../../graphql/Mutations'
+import {FormEvent} from 'react'
 
 export const useCreateTask = (weekDaysList: string[]) => {
-  const [createTask] = useMutation(CREATE_TASK_MUTATION)
-  const onSubmitTask = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    const inputsData = Object.fromEntries(formData)
-    inputsData.owner = localStorage.getItem('user') as string
-    createTask({
-      variables: {
-        taskInput: inputsData,
-      },
+    const [createTask] = useMutation(CREATE_TASK_MUTATION)
+    const onSubmitTask = (event: FormEvent<HTMLFormElement>) => {
+        const target = event.target as HTMLFormElement
+        const formData = new FormData(target)
+        const inputsData = Object.fromEntries(formData)
 
-      update(cache) {
-        cache.modify({
-          fields: {
-            tasks: (existingData) => {
-              return [...existingData, inputsData]
+        inputsData.owner = localStorage.getItem('user') as string
+        createTask({
+            variables: {
+                taskInput: inputsData,
             },
-          },
-        })
-      },
-    })
-  }
 
-  return {
-    onSubmitTask,
-  }
+            update(cache) {
+                cache.modify({
+                    fields: {
+                        tasks: (existingData) => {
+                            return [...existingData, inputsData]
+                        },
+                    },
+                })
+            },
+        })
+    }
+
+    return {
+        onSubmitTask,
+    }
 }
